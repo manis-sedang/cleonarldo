e87c28b8e59f308c87924088d3a9c72a47f9078f
manis-sedang <gevelinanesta46@gmail.com>
2022-09-26 18:58:49 +0000

    init

---
+++ spec/cleonarldo-view-spec.js
@@ -0,0 +1,9 @@
+'use babel';
+
+import CleonarldoView from '../lib/cleonarldo-view';
+
+describe('CleonarldoView', () => {
+  it('has one valid test', () => {
+    expect('life').toBe('easy');
+  });
+});
